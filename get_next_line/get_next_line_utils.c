a2972147e19a24256b28576889ca3560f03dca2b
mohamed elmhassani <mohamed_elmhassani@live.fr>
2022-10-23 19:53:54 +0100

    get next line init

---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: melmhass <melmhass@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/23 16:36:53 by melmhass          #+#    #+#             */
+/*   Updated: 2022/10/23 18:30:15 by melmhass         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (*s++)
+		len++;
+	return (len);
+}
+
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	len;
+
+	len = (int)ft_strlen(s);
+	while (len >= 0)
+	{
+		if (s[len] == (char)c)
+			return ((char *)(s + len));
+		len--;
+	}
+	return (NULL);
+}
+
+
